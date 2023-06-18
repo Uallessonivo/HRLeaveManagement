69caa707acb64ca5c549e863e7da57af179e1a42
Uallesson Nunes Ivo <uallessons@gmail.com>
2023-06-18 08:17:02 -0300

    feat(persistence): implemeting the repos and repository pattern

---
+++ HRLeaveManagement.Persistence/Repositories/LeaveAllocationRepository.cs
@@ -0,0 +1,25 @@
+﻿using HRLeaveManagement.Application.Persistence.Contracts;
+using HRLeaveManagement.Domain;
+
+namespace HRLeaveManagement.Persistence.Repositories
+{
+    public class LeaveAllocationRepository : GenericRepository<LeaveAllocation>, ILeaveAllocationRepository
+    {
+        private readonly LeaveManagementDbContext _dbContext;
+
+        public LeaveAllocationRepository(LeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<List<LeaveAllocation>> GetLeaveAllocationsWithDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveAllocation> GetLeaveAllocationWithDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HRLeaveManagement.Persistence/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,25 @@
+﻿using HRLeaveManagement.Application.Persistence.Contracts;
+using HRLeaveManagement.Domain;
+
+namespace HRLeaveManagement.Persistence.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        private readonly LeaveManagementDbContext _dbContext;
+
+        public LeaveTypeRepository(LeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<List<LeaveType>> GetLeaveTypesWithDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LeaveType> GetLeaveTypeWithDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
